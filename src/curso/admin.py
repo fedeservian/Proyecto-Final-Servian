4578001932a059459b4b925a7583124350fc94f4
Federico Servian <fmservian@gmail.com>
2024-12-10 22:20:54 -0300

    Modelos Listos

--- src/curso/admin.py
+++ src/curso/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Category, Product, ProductionOrder, Inventory
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cost_price', 'production_time', 'category')  
+    search_fields = ('name', 'description')  
+
+@admin.register(ProductionOrder)
+class ProductionOrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'product', 'status', 'quantity')  
+    search_fields = ('order_number',)  
+
+@admin.register(Inventory)
+class InventoryAdmin(admin.ModelAdmin):
+    list_display = ('product', 'location', 'stock_quantity')  
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')  
+    search_fields = ('name',)  
+

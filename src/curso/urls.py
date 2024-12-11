34885a49dcf0d2782ba55a2bc9e71bba732a2355
Federico Servian <fmservian@gmail.com>
2024-12-11 18:01:13 -0300

    URLs y enlaces de listas agregados

--- src/curso/urls.py
+++ src/curso/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about, category_list, product_list, production_order_list, inventory_list
 
 app_name = "curso"
 
 urlpatterns = [
     path("", index, name="index"),
     path("about/", about, name="about"),
+    path("category/list/", category_list, name="category_list"),
+    path("product/list/", product_list, name="product_list"),
+    path("production_order/list/", production_order_list, name="production_order_list"),
+    path("inventory/list/", inventory_list, name="inventory_list"),
 ]
